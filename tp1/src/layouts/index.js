f3cf3001f2d80a5b3996128d8a554b303da81fb1
Angelo <angeloraso6@gmail.com>
2018-08-26 15:18:12 -0300

    Cambia estilos del Footer

--- tp1/src/layouts/index.js
+++ tp1/src/layouts/index.js
@@ -11,19 +11,30 @@ const ListLink = props =>
   </li>
 
 export default ({ children }) =>
-  <div style={{ margin: `0 auto`, maxWidth: 650, padding: `1.25rem 1rem` }}>
-    <header style={{ marginBottom: `1.5rem`}}>
-      <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-        <h3 style={{ display: `inline` }}>PARKER</h3>
-      </Link>
-      <ul style={{ listStyle: `none`, float: `right` }}>
-        <ListLink to="/">Grupo</ListLink>
-        <ListLink to="/plumas/">Plumas</ListLink>
-        <ListLink to="/lapiceras/">Lapiceras</ListLink>
-      </ul>
-    </header>
-    {children()}
-    <footer>
+  <div>
+    <div css={{ margin: `0 auto`, maxWidth: 650, padding: `1.25rem 1rem` }}>
+        <header style={{ marginBottom: `1.5rem`}}>
+          <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+            <h3 style={{ display: `inline`}}>PARKER</h3>
+          </Link>
+          <ul style={{ listStyle: `none`, float: `right`}}>
+            <ListLink to="/">Grupo</ListLink>
+            <ListLink to="/plumas/">Plumas</ListLink>
+            <ListLink to="/lapiceras/">Lapiceras</ListLink>
+          </ul>
+        </header>
+        {children()}
+    </div>
+    <footer css={{ 
+                   color: 'whitesmoke',
+                   background: '#999',
+                   paddingLeft: '30%',
+                   paddingTop: '5px',
+                   width: '100%',
+                   height: '40px',
+                   bottom: '0px',
+                   position: 'fixed',
+                }}>
       Página diseñada para la cátedra de Aplicaciones Móviles - Año 2018
     </footer>
   </div>
